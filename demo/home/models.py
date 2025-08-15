604628771a59a5783496049d7b671aafdd1ff0e6
Bepi64 <borham.sakr@epita.fr>
2025-08-15 19:10:28 +0200

    a

--- demo/home/models.py
+++ demo/home/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 # Create your models here.
-class HumanMessage(models.Model):
-    author = models.CharField(max_length=20)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    ai_receiver = models.CharField(max_length=100)
 
-class BotMessage(models.Model):
-    ai_sender = models.CharField(max_length=100)
+class Session(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Message(models.Model):
+    sender = models.CharField(max_length=20)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    receiver = models.CharField(max_length=100)
+    session = models.ForeignKey(Session, related_name='messages', on_delete=models.CASCADE)
--- demo/home/views.py
+++ demo/home/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render, HttpResponse
 from ollama import list
+import multiprocessing as mp
+import subprocess
+
+processs = []
+
+def load_ollama():
+    try:
+        print("Loading Ollama...")
+        subprocess.run(['ollama', 'serve'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error loading Ollama: {e}")
+
+def end_ollama(p):
+    if p.is_alive():
+        print("Stopping Ollama...")
+        p.terminate()
+    else:
+        print("Ollama process is not running.")
+
 
 # Create your views here.
 def home(request):
+    p = mp.Process(target=load_ollama)
+    p.start()
+    processs.append(p)
     return render(request, 'index.html')
 
 def model(request):
